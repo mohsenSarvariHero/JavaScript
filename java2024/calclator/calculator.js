7f77f05e035c23e566fb6e8485a65097d0fb0505
mohsenSarvariHero <91257860+mohsenSarvariHero@users.noreply.github.com>
2024-11-11 15:13:58 +0330

    Add files via upload

---
+++ java2024/calclator/calculator.js
@@ -0,0 +1,16 @@
+console.log("hello")
+
+const numbers = [];
+const show = (x) => {
+    numbers.push(x)
+    numbers.forEach((item)=>{
+    console.log(item)
+    })
+}
+const result = (x) => {
+    numbers.forEach((item)=>{
+        if(item == '+')
+            num
+    let num1 = item
+    })
+}
